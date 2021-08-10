f3e82c8cf49924ec81ab252dd0d8d636e19d0d6c
KFC <chris.chan@greenpeace.org>
2021-08-10 16:29:04 +0800

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
+// Prettier configuration
+// https://prettier.io/docs/en/configuration.html
 module.exports = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'all',
   jsxSingleQuote: true,
 }
