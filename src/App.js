13155b9ce33b645a19cd7eee476a4f25da33d61a
CK Lam <ckhk0621@gmail.com>
2021-08-12 16:23:24 +0800

    updated sticky

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import { HelmetProvider } from 'react-helmet-async'
+import Sticky from 'react-sticky-el';
 import SEO from './components/seo'
 import theme from './theme'
 import Form from './components/form'
@@ -53,26 +54,36 @@ const App = (props) => {
               <Center h={'100%'}>
                 <Stack direction="column">
                   {/* <Text fontSize={'24px'} fontWeight={500} color='gray.700'>急需你參與「管制即棄膠餐具計劃」公眾諮詢</Text> */}
-                  <Image src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`} maxW={'480px'}/><br/>
                   <Countdown/>
-                  {/* <Box pos='absolute' {...rightBottomCorner}></Box> */}
+                  <Image src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`} maxW={'480px'}/><br/>
+                  <Image src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`} maxW={'480px'}/><br/>
+                  <Image src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`} maxW={'480px'}/><br/>
+                  <Image src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`} maxW={'480px'}/><br/>
+                  <Image src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`} maxW={'480px'}/><br/>
                 </Stack>
               </Center>
             </Box>
-            <Box w="100%" h="80vh" bg="#F5F5F5" position={'relative'}>
-              <Center>
-                <Box maxW={'540px'}>
-                  <Form/>
+            <Box w="100%" h="100%" bg="#F5F5F5" position={'relative'} className="rightBlock">
+              <Sticky stickyStyle={{zIndex: 10}} bottomOffset={20} boundaryElement=".rightBlock" hideOnBoundaryHit={false}>
+                <Box px={{base: 0, sm: 20}}>
+                <Form/>
                 </Box>
-              </Center>
-              <Box pos={'absolute'} bottom={0} right={0} p={6}>
+              </Sticky>
+              {/* <Center>
+                <Box maxW={'540px'} pos={'relative'}>
+                  <Box>
+                  
+                  </Box>
+                </Box>
+              </Center> */}
+              {/* <Box pos={'absolute'} bottom={0} right={0} p={6}>
                 <Stack direction="row" color={'gray.500'} alignItems={'center'}>
                   <Box>步驟</Box>
                   <Box bgColor={'green.300'} w={'40px'} h={'40px'} borderRadius={'50%'} fontWeight={700} fontSize={18} color="#FFF" textAlign={'center'} fontFamily={'arial'} lineHeight={'40px'}>1</Box>
                   <Box fontFamily={'arial'}>2</Box>
                   <Box fontFamily={'arial'}>3</Box>
                 </Stack>
-              </Box>
+              </Box> */}
             </Box>
           </Grid>
           </Box>
