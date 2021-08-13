22db2762e359fee200f2ae5d143722411766d2b9
KFC <chris.chan@greenpeace.org>
2021-08-13 12:37:47 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react'
 import {
   ChakraProvider,
   Box,
-  VStack,
   Center,
-  Flex,
   Heading,
   Image,
   Stack,
@@ -22,7 +20,6 @@ import Nav from './components/Nav'
 import Countdown from './components/Countdown'
 import Testimonials from './components/Testimonials'
 import SplitWithImage from './components/SplitWithImage/SplitWithImage'
-import GridListWithHeading from './components/GridList/GridListWithHeading'
 import LargeWithLogoLeft from './components/Footer/LargeWithLogoLeft'
 import SmallWithSocial from './components/Footer/SmallWithSocial'
 
@@ -46,8 +43,8 @@ const ForkWrapper = styled.div`
   position: absolute;
   right: -20%;
   bottom: -20%;
-  width: 500px;
-  height: 500px;
+  width: 80%;
+  height: 80%;
   > img {
     opacity: 0.5;
     position: absolute;
@@ -81,18 +78,44 @@ const App = (props) => {
                     >
                       <Countdown />
                       <Heading
+                        fontSize={{ base: '2xl', sm: '4xl' }}
+                        lineHeight={'130%'}
+                      >
+                        <Text mb={1} fontSize={{ base: '5xl' }}>
+                          急需您的參與
+                        </Text>
+                        <Text color={'#ff8100'} fontWeight={'700'}>
+                          管制即棄膠餐具計劃
+                        </Text>
+                        <Text>公眾諮詢</Text>
+                      </Heading>
+                      <Text color={'gray.700'} lineHeight={'200%'}>
+                        環保署已經展開「管制即棄膠餐具計劃」公眾諮詢，截止日期為9月8日。立即行動填妥綠色和平網站表格，參與公眾諮詢，推動香港走塑進程！
+                      </Text>
+                      <ForkWrapper>
+                        <Image src={Fork} objectFit='contain' />
+                      </ForkWrapper>
+                    </Stack>
+                  </Center>
+                  <Center h={'80vh'}>
+                    <Stack
+                      as={Box}
+                      h={'100%'}
+                      spacing={6}
+                      py={{ base: 8, md: 12 }}
+                      px={4}
+                      position={'relative'}
+                      alignItems={'flex-start'}
+                    >
+                      <Heading
                         fontWeight={'bold'}
                         fontSize={{ base: '2xl', sm: '4xl' }}
                         lineHeight={'120%'}
                       >
-                        <Text>急需您的參與</Text>
-                        <Text color={'#ff8100'}>管制即棄膠餐具計劃</Text>
-                        <Text>公眾諮詢</Text>
+                        <Text>電郵您的意見書範本</Text>
                       </Heading>
                       <Text color={'gray.700'} lineHeight={'200%'}>
-                        環保署已經展開「管制即棄膠餐具計劃」公眾諮詢，截止日期為9月8日。
-                        <br />
-                        立即行動填妥綠色和平網站表格，參與公眾諮詢，推動香港走塑進程！
+                        開啟你的電子郵箱，打開由綠色和平發出，附有意見書範本的電郵。轉寄該電郵，在「收件人」一欄輸入rdpt@epd.gov.hk，按下發送。
                       </Text>
                       <ForkWrapper>
                         <Image src={Fork} objectFit='contain' />
@@ -108,22 +131,6 @@ const App = (props) => {
                     src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`}
                     maxW={'480px'}
                   />
-                  <br />
-                  <Image
-                    src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`}
-                    maxW={'480px'}
-                  />
-                  <br />
-                  <Image
-                    src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`}
-                    maxW={'480px'}
-                  />
-                  <br />
-                  <Image
-                    src={`${process.env.PUBLIC_URL}/assets/20210805_RDPT_KV-02.png`}
-                    maxW={'480px'}
-                  />
-                  <br />
                 </Stack>
               </Center>
             </Box>
@@ -151,7 +158,6 @@ const App = (props) => {
           <Testimonials />
         </Box>
         <SplitWithImage />
-        {/* <GridListWithHeading /> */}
         <Footer>
           <LargeWithLogoLeft />
           <SmallWithSocial />
